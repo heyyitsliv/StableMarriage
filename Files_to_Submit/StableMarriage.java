d3a58ba5e14d16d2b25a54382ef34b6095b318b1
Alexis <70601637+logan145@users.noreply.github.com>
2026-05-28 20:21:58 -0700

    Update StableMarriage.java

--- Files_to_Submit/StableMarriage.java
+++ Files_to_Submit/StableMarriage.java
@@ -1,18 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class StableMarriage {
     public static void main(String[] args) {
         // call run test method with file as arument
+        runTest("matchmaking.txt");
     }
 
     // run test method (imports the file as an argument)
     public static void runTest(String filename) {
         // read input file
+        ArrayList<String[]> fileLines = new ArrayList<>();
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (!line.isEmpty()){
+                    fileLines.add(line.split("\\s+"));
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File " + filename + " not found.");
+                return;
+        }
+                        
         // creates persons from person class
-        // creates an ArrayList for males
+        ArrayList<Person> allPerson = new ArrayList<>();
+        for (String[] parts : fileLines) {
+            String name = parts[0];
+            char gender = parts[1].toUpperCase().charAt(0);
+            ArrayList<Integer> preferences = new ArrayList<>();
+            for (int i = 2; i < parts.length; i++) {
+                preferences.add(Integer.parseInt(parts[i]));
+            }
+            //added
+            allPerson.add(new Person(name, preferences, gender));
+        }
+ // creates an ArrayList for males
+        ArrayList<Person> males = new ArrayList<>();
+        
         // creates an ArrayList for females
+        ArrayList<Person> females = new  ArrayList<>();
+            //added
+            for (int i = 0; i < allPerson.size(); i++) {
+            char gender = fileLines.get(i)[1].toUpperCase().charAt(0);
+            if (gender == 'M') {
+                males.add(allPerson.get(i));
+            } else if (gender == 'F') {
+                females.add(allPerson.get(i));
+            }
+        }
+        
+        
         // calls stable pairings method
+        //stablePairings(males, females);
+        
         // call print output method
+        //printOutput(males, females);
     }
 
     // generate stable pairings method (pass in two ArrayLists of men and women)
